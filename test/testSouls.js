d3f37ef041a4ff1c17cca9afb9aa24079f7ebca4
Frank Polster <frank.polster@maibornwolff.de>
2020-10-24 12:03:16 +0200

    further testing

--- test/testSouls.js
+++ test/testSouls.js
@@ -9,11 +9,22 @@ contract("Souls", (accounts) => {
     // should have enough Souls after deployment (msg.sender)
     const accountWithSoulsAndCthu = accounts[0];
 
+    let initCthuTokenBalance;
+    let soulsRewardsBalance;
+    let soulsBalance;
+
     before(async () => {
         // init contracts
         deployedSOULS = await SOULS.new();
         deployedCTHU = await CTHU.new("cthu", "cthu", "18", "20000000000000000000000000", "10000000000000000000000000", true, false);
         await deployedSOULS.setCthuAddress.sendTransaction(deployedCTHU.address);
+
+        initCthuTokenBalance = await deployedCTHU.balanceOf.call(accountWithSoulsAndCthu);
+    })
+
+    it("provides the souls token balance of a specific address", async () => {
+        const balanceOfUser = await deployedSOULS.balanceOf.call(accountWithSoulsAndCthu);
+        assert.equal(balanceOfUser.toString(), "1000000000000000000000");
     })
 
     it("provides stake function which transfers souls to souls contract", async () => {
@@ -28,33 +39,77 @@ contract("Souls", (accounts) => {
         assert.equal(response.toString(), "10000000000000000000000000");
     })
 
-    it("allows to withdraw staked chtu from souls contracts", async () => {
-        // generate some blocks 
+    it("allows to generate rewards on each block", async () => {
+        // generate some blocks and rewards
+
+        // 10
         await web3.eth.sendTransaction({ to: accounts[1], from: accounts[0] })
+        soulsRewardsBalance = await deployedSOULS.myRewardsBalance.call(accountWithSoulsAndCthu);
+        assert.equal(soulsRewardsBalance.toString(), "100000000000000000000")
+
+        // 20
         await web3.eth.sendTransaction({ to: accounts[1], from: accounts[0] })
+        soulsRewardsBalance = await deployedSOULS.myRewardsBalance.call(accountWithSoulsAndCthu);
+        assert.equal(soulsRewardsBalance.toString(), "200000000000000000000")
+
+        // 30
         await web3.eth.sendTransaction({ to: accounts[1], from: accounts[0] })
-        await web3.eth.sendTransaction({ to: accounts[1], from: accounts[0] })
-        const response = await deployedSOULS.withdraw.sendTransaction("10000000000000000000000000");
-        const balanceOfUser = await deployedCTHU.balanceOf.call(accountWithSoulsAndCthu);
-        assert.equal(balanceOfUser.toString(), "10000000000000000000000000");
+        soulsRewardsBalance = await deployedSOULS.myRewardsBalance.call(accountWithSoulsAndCthu);
+        assert.equal(soulsRewardsBalance.toString(), "300000000000000000000");
     })
 
-    it("provides the souls token balance of a specific address", async () => {
-        const balanceOfUser = await deployedSOULS.balanceOf.call(accountWithSoulsAndCthu);
-        assert.equal(balanceOfUser.toString(), "1000000000000000000000");
-    })
+    it("provides get reward function which returns farmed souls", async () => {
+        // available rewards 30
+        soulsRewardsBalance = await deployedSOULS.myRewardsBalance.call(accountWithSoulsAndCthu);
+        assert.equal(soulsRewardsBalance.toString(), "300000000000000000000");
 
-    it("provides function which returns available rewards balance", async () => {
-        // get actual rewards available
-        const balanceOfUser = await deployedSOULS.myRewardsBalance.call(accountWithSoulsAndCthu);
-        assert.notEqual(balanceOfUser.toString(), "0"); // why this returns 0?
-    })
+        // current souls balance 100
+        soulsBalance = await deployedSOULS.balanceOf.call(accountWithSoulsAndCthu);
+        assert.equal(soulsBalance.toString(), "1000000000000000000000")
 
-    it("provides getReward function which transfers souls to msg sender", async () => {
-        const balanceOfUserPreGetReward = await deployedSOULS.balanceOf.call(accountWithSoulsAndCthu);
+        // get reward
         await deployedSOULS.getReward.sendTransaction();
-        const balanceOfUserPostGetReward = await deployedSOULS.balanceOf.call(accountWithSoulsAndCthu);
-        assert.notEqual(balanceOfUserPreGetReward.toString(), balanceOfUserPostGetReward.toString());
+
+        // new balance is 136  
+        soulsBalance = await deployedSOULS.balanceOf.call(accountWithSoulsAndCthu);
+        assert.equal(soulsBalance.toString(), "1360000000000000000000")
+
+        await deployedSOULS.getReward.sendTransaction();
+        soulsBalance = await deployedSOULS.balanceOf.call(accountWithSoulsAndCthu);
+        assert.equal(soulsBalance.toString(), "1450000000000000000000")
+
+        // call  get reward second time new Balance 145
+        await deployedSOULS.getReward.sendTransaction();
+        soulsBalance = await deployedSOULS.balanceOf.call(accountWithSoulsAndCthu);
+        assert.equal(soulsBalance.toString(), "1540000000000000000000")
+    })
+
+    it("allows to withdraw once which unstakes cthu and harvest rewards", async () => {
+        // generate reward
+        await web3.eth.sendTransaction({ to: accounts[1], from: accounts[0] })
+
+        // get balances before withdraw
+        const rewardsBalance = await deployedSOULS.myRewardsBalance.call(accountWithSoulsAndCthu);
+        const stakedBalance = await deployedSOULS.getAddressStakeAmount.call(accountWithSoulsAndCthu);
+
+        // withdraw
+        await deployedSOULS.withdraw.sendTransaction(initCthuTokenBalance.toString());
+
+        // get balances after withdraw
+        const newBalance = await deployedCTHU.balanceOf.call(accountWithSoulsAndCthu);
+        const newSoulsBalance = await deployedSOULS.balanceOf.call(accountWithSoulsAndCthu);
+        const newStakedAmount = await deployedSOULS.getAddressStakeAmount.call(accountWithSoulsAndCthu);
+        const newRewardsBalance = await deployedSOULS.myRewardsBalance.call(accountWithSoulsAndCthu);
+
+        // should return all staked balance
+        assert.equal(stakedBalance.toString(), newBalance.toString());
+
+        // new  stake amount is 0
+        assert.equal(newStakedAmount.toString(), "0");
+
+        // should withdraw open rewards @TODO
+        assert.equal(newSoulsBalance.toString(), "1640000000000000000000"); // actual 1540000000000000000000
+        assert.equal(newRewardsBalance.toString(), "0");
     })
 
 })
